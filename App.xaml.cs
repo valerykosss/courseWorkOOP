4d57461596e167808b368c64ac55d020c860ca0b
valerykosss <90275681+valerykosss@users.noreply.github.com>
2022-06-30 04:50:19 +0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using CLINICS.models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CLINICS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+
+    public partial class App : Application
+    {
+        public static CLIENT currentClient { get; set; }
+        public App()
+        {
+            App.currentClient = new CLIENT();
+        }
+    }
+}
